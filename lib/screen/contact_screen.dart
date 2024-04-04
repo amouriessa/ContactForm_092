7f5c87908e11ab048e2a871f70c050eeda0feaeb
Bagas Saras <sarasbagas63@gmail.com>
2024-04-04 15:34:11 +0700

    Add Statefulwidget on contact_screen file

---
+++ lib/screen/contact_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ContactScreen extends StatefulWidget {
+  const ContactScreen({super.key});
+
+  @override
+  State<ContactScreen> createState() => _ContactScreenState();
+}
+
+class _ContactScreenState extends State<ContactScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
