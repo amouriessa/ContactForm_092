1d7a660206189e24ae592747c318e11883c66bfa
Bagas Saras <sarasbagas63@gmail.com>
2024-04-04 15:36:52 +0700

    Add contact screen state and form

--- lib/screen/contact_screen.dart
+++ lib/screen/contact_screen.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ContactScreen extends StatefulWidget {
   const ContactScreen({super.key});
@@ -9,8 +10,72 @@ class ContactScreen extends StatefulWidget {
 }
 
 class _ContactScreenState extends State<ContactScreen> {
+  File? _image;
+  final _imagePicker = ImagePicker();
+
+  final formkey = GlobalKey<FormState>();
+  final _namaController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _alamatController = TextEditingController();
+  final _noTelpController = TextEditingController();
+
+  Future<void> getImage() async {
+    final XFile? pickerFile =
+        await _imagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickerFile != null) {
+        _image = File(pickerFile.path);
+      } else {
+        print("Please Input Image First!");
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Form(
+        key: formkey,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "Name", hintText: "Enter Contact Name"),
+                  controller: _namaController,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "Email", hintText: "Enter Contact Email"),
+                  controller: _emailController,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "Address", hintText: "Enter Contact Address"),
+                  controller: _alamatController,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "Number", hintText: "Enter Contact Number"),
+                  controller: _noTelpController,
+                ),
+              ),
+              _image == null
+                  ? const Text('No Image Selected.')
+                  : Image.file(_image!)
+            ],
+          ),
+        ));
   }
 }
